d2f1af470f022c878f506d761a65b3de26f11bce
emilykidd3 <e.kidd61@yahoo.com>
2021-04-10 13:22:40 -0700

    created all models

--- models/User.js
+++ models/User.js
@@ -1,36 +1,44 @@
-const { schema, model, Schema } = require('mongoose');
-const { builtinModules } = require('node:module');
+const { schema, model, Schema } = require("mongoose");
 
 const UserSchema = new Schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            required: 'Please enter a username',
-            trim: true
-        },
-        email: {
-            Type: String,
-            required: true,
-            unique: true,
-            match: [/.+\@.+\..+/, "Please enter a valid email address"]
-        },
-        thoughts: [ThoughtSchema],
-        friends: [UserSchema],
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: "Please enter a username",
+      trim: true,
     },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
+    email: {
+      Type: String,
+      required: true,
+      unique: true,
+      match: [/.+\@.+\..+/, "Please enter a valid email address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
-UserSchema.virtual('friendCount').get(function() {
-    return this.friends.length;
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
-const User = model('User', UserSchema);
+const User = model("User", UserSchema);
 
 module.exports = User;
