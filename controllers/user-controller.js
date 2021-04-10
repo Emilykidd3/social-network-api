45dd71abfcaa59a128eaa6b5947e6be6ade0fe27
emilykidd3 <e.kidd61@yahoo.com>
2021-04-10 13:59:29 -0700

    added function for finding one user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,11 +12,30 @@ const userController = {
             .then(dbUserData => res.json(dbUserData))
             .catch(err => {
                 console.log(err);
-                res.status(400).json(err);
+                res.status(500).json(err);
             });
     },
 
     // get one use by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            })
+    }
 }
 
 module.exports = userController;
