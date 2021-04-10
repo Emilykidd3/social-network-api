f39d4e0a8172f119a011cc99f8321265613eccd3
emilykidd3 <e.kidd61@yahoo.com>
2021-04-10 13:41:01 -0700

    added get all users in user controller

---
+++ routes/api/user-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers
+} = require('../../controllers/user-controller');
+
+// get all
+router.route('/').get(getAllUsers);
+
+module.exports = router;
